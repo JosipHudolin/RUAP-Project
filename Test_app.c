806c8fbfc992385268e296daca060a237b801b62
renatabarisic <rbarisic@etfos.hr>
2023-11-03 17:47:44 +0100

    Drugi commit

--- Test_app.c
+++ Test_app.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Pozdrav iz aplikacije od JOsipa Hudolina");
+    printf("Pozdrav iz aplikacije od JOsipa Hudolina s brojem indeksa 6");
     return 0;
 }
